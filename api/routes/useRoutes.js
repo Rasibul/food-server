755766ae48fc4434e95a9d0d7e7948329fd87ea0
Rasibul <rasibul179@gmail.com>
2024-01-02 22:03:48 +0600

    create a verify admin route and set all secure route

--- api/routes/useRoutes.js
+++ api/routes/useRoutes.js
@@ -2,11 +2,12 @@ const express = require('express')
 const router = express.Router()
 const userControler = require('../controlers/userControler')
 const verifyToken = require('../middelware/verifyToken')
+const verifyAdmin = require('../middelware/verifyAdmin')
 
-router.get('/', userControler.getAllUsers)
+router.get('/', verifyToken, verifyAdmin, userControler.getAllUsers)
 router.post('/', userControler.createUser)
-router.delete('/:id', userControler.deleteUser)
-router.get('/admin/:email', userControler.getAdmin)
-router.patch('/admin/:id', userControler.makeAdmin)
+router.delete('/:id', verifyToken, verifyAdmin, userControler.deleteUser)
+router.get('/admin/:email', verifyToken, userControler.getAdmin)
+router.patch('/admin/:id', verifyToken, verifyAdmin, userControler.makeAdmin)
 
 module.exports = router
